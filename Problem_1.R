881ae3208e13c4c35a7879e0f161597a723c6afb
mtraykova <37689700+mtraykova@users.noreply.github.com>
2018-04-02 17:32:10 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,64 @@
+#####################
+
+#Homework 1, Problem 1
+
+#a)
+
+x <- c(4,1,1,4)
+x
+
+#b) 
+
+y <- c(1,4)
+
+#c)
+
+x-y
+
+#This command substracts y from x. Because x is with length 4 and y with length 2, in order to complete the command properly, y is cloned so as to have length of 4. This way y becomes (1,4,1,4) With this command, the first number os x and y are substracted, then the second ect.
+
+#d)
+
+s <- c(x,y)
+
+s
+
+#e)
+
+s1 <- rep(s, 10)
+
+s1
+
+length(s1)
+
+
+#f)
+
+s2 <- rep(s, each=3)
+
+s2
+
+#g)
+  #i)
+
+seq(from=7,to=21,by=1)
+
+  #ii)
+
+7:21
+
+#h)
+
+length(seq(from=7,to=21,by=1))
+
+
+
+##########################
+
+
+
+
+
+
+
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,89 @@
+####################
+
+#Homework 1, Problem 2
+
+
+#a)
+
+date <- c('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+
+
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+
+xmin
+
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+xmax
+
+
+#b)
+
+xmax-xmin
+
+
+#c)
+
+avgMin <- sum(xmin)/length(xmin)
+
+avgMin
+
+avgMax <- sum(xmax)/length(xmax)
+
+avgMax
+
+
+
+#d)
+
+xmin[xmin <= avgMin]
+
+
+#e)
+
+xmin[xmax>=avgMax]
+
+
+#f)
+
+names(xmin) <- date
+
+names(xmax) <- date
+
+xmin
+
+xmax
+
+
+
+#g)
+
+temperatures <- data.frame(Min=xmin, Max=xmax)
+
+temperatures
+
+
+
+#h)
+
+xminFahrenheit <- c(xminFahrenheit=(9/5*xmin+32))
+
+xminFahrenheit
+
+
+
+#i)
+
+temperatures <- data.frame(Min=xminFahrenheit, Max=xmax)
+
+temperatures
+
+
+
+#j)
+
+temperatures <- data.frame(Min=xminFahrenheit, Max=xmax, Date=date('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18'))
+
+temperatures
+
 
